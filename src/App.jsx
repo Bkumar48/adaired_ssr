d12dd879f9e077f1544ea0d22f916206295da63a
Kumar <bittu.kumar@adaired61.com>
2024-01-31 19:41:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.scss";
-import { useEffect, useState } from "react";
 import Layout from "./views/frontend/Layout/Index";
-import SplashScreen from "./views/frontend/Components/SplashScreen/Index";
-import { gsap, CSSPlugin, Expo } from "gsap";
-gsap.registerPlugin(CSSPlugin);
+import { StaticRouter } from "react-router-dom/server";
 
 function App() {
   return (
-    <div className="app">
-      <SplashScreen />
-      <div className="content">
-        {/* <Layout /> */}
-      </div>
-    </div>
+    <>
+      <StaticRouter>
+        <Layout />
+      </StaticRouter>
+    </>
   );
 }
 
